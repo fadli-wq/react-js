55895d66bd91aed267ecc063cb6506d65d0929e7
fadli-wq <dayfos123@gmail.com>
2024-06-04 21:41:30 +0700

    hapus / di src img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import { TypeAnimation } from 'react-type-animation'
 const Main = () => {
   return (
     <div id='main'>
-        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="/src/assets/img/aincrad.jpg" alt="aincrad" />
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="src/assets/img/aincrad.jpg" alt="aincrad" />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start item-center'>
                 <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Saya Muhammad Fahli Aziz</h1>
